257a9ff8956f325aa87ab5a2f27d7f46c30fc4d6
Roshan Kenia <keniaroshan@gmail.com>
2022-07-17 15:50:09 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -20,20 +20,20 @@ else:
 
 def loss_coteaching(y_1, y_2, t, forget_rate, ind, noise_or_not):
     loss_1 = F.cross_entropy(y_1, t, reduce=False)
-    ind_1_sorted = np.argsort(loss_1.data.cpu()).cuda()
+    ind_1_sorted = np.argsort(loss_1.data.cpu())
     loss_1_sorted = loss_1[ind_1_sorted]
 
     loss_2 = F.cross_entropy(y_2, t, reduce=False)
-    ind_2_sorted = np.argsort(loss_2.data.cpu()).cuda()
+    ind_2_sorted = np.argsort(loss_2.data.cpu())
     loss_2_sorted = loss_2[ind_2_sorted]
 
     remember_rate = 1 - forget_rate
     num_remember = int(remember_rate * len(loss_1_sorted))
 
     pure_ratio_1 = np.sum(
-        noise_or_not[ind[ind_1_sorted[:num_remember]]].cpu())/float(num_remember)
+        noise_or_not[ind[ind_1_sorted[:num_remember]]])/float(num_remember)
     pure_ratio_2 = np.sum(
-        noise_or_not[ind[ind_2_sorted[:num_remember]]].cpu())/float(num_remember)
+        noise_or_not[ind[ind_2_sorted[:num_remember]]])/float(num_remember)
 
     ind_1_update = ind_1_sorted[:num_remember]
     ind_2_update = ind_2_sorted[:num_remember]
